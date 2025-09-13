eb67919de49fdf061e515d5e90de420fcafa95db
Garcaaaa <juandigar2006@gmail.com>
2025-09-13 15:35:21 -0300

    Equipo casi pronto

--- simuladorequipo1/src/main/java/com/example/Interfaces/IEquipo.java
+++ simuladorequipo1/src/main/java/com/example/Interfaces/IEquipo.java
@@ -1,5 +1,13 @@
 package com.example.Interfaces;
 
-public interface IEquipo {
+import com.example.ClasesProyecto.Jugador;
 
+public interface IEquipo <Jugador extends Comparable <Jugador>> {
+    Boolean estaVacio ();
+    Jugador borrarJugador (String id);
+    Boolean agregarJugador (Jugador jugador);
+    Jugador buscarJugador (Jugador j);
+    int cantidadDeJugadores();
+    void mostrarJugadores();
 }
+
