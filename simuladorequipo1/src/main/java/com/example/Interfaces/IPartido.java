50a41b98dd1577b757db7c6e717b314b7730eb4d
Nicolas Vazquez <Nicolas Vazquez>
2025-09-15 17:47:39 -0300

    Nuevos arreglos de la clase partido

--- simuladorequipo1/src/main/java/com/example/Interfaces/IPartido.java
+++ simuladorequipo1/src/main/java/com/example/Interfaces/IPartido.java
@@ -5,29 +5,17 @@ import com.example.lista.TDALista;
 import java.time.LocalDateTime;
 
 public interface IPartido {
-    public interface Partido {
 
-
-
-        interface Gol extends Comparable<Gol> {
+    Gol extends Comparable<Gol> {
             String getEquipo();
             String getJugador();
             int getMinuto();
         }
-
-        interface Resultado extends Comparable<Resultado> {
-            String getLocal();
-            String getVisitante();
-            int getGolesLocal();
-            int getGolesVisitante();
-            java.time.LocalDateTime getFecha();
-        }
-
-        interface EstadisticaEquipo extends Comparable<EstadisticaEquipo> {
-            String getEquipo();
-            int getGolesAnotados();
-            int getGolesRecibidos();
-        }
+    Resultado extends Comparable<Resultado> {
+        String getLocal();
+        String getVisitante();
+        int getGolesLocal();
+        int getGolesVisitante();
+        java.time.LocalDateTime getFecha();
     }
-
 }
