66383cdd4fd33c81d21a8e907704d5bf673b1f05
Garcaaaa <juandigar2006@gmail.com>
2025-09-16 11:41:42 -0300

    TDA que faltaban

--- simuladorequipo1/src/main/java/com/example/lista/impl/PilaListaEnlazada.java
+++ simuladorequipo1/src/main/java/com/example/lista/impl/PilaListaEnlazada.java
@@ -1,5 +1,33 @@
 package com.example.lista.impl;
 
-public class PilaListaEnlazada {
+import com.example.lista.TDAPila;
 
+public class PilaListaEnlazada < T extends Comparable<T>> extends TDAListaEnlazada<T> implements TDAPila<T> {
+    /*No hacemos otra cabeza, usamos la q ya tiene TDAListaEnlazada, 
+    porque sino estamos haciendo 2 cabezas distintas y no funcionan los metodos*/
+
+    //sirve para insertar un elemento en la pila
+    public void push(T data) {
+        cabeza = new Nodo<T>(data, cabeza);
+    }
+
+    //seria como el peek
+    @Override
+    public T tope() {
+        if (esVacia()){
+            return null;
+        }
+        return cabeza.getDato();
+    }
+
+    //Sirve para sacar un elemento de la pila
+    @Override
+    public T sacar() {
+        if (esVacia()){
+            return null;
+        }
+        T dato = cabeza.getDato();
+        cabeza = cabeza.getSiguiente();
+        return dato;
+    }
 }
